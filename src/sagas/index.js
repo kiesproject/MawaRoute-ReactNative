995faaeff745923e79fd1af75b8af6b42433dfb3
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-02 17:12:04 +0900

    Add Permission logic for Android

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,4 @@
+import { PermissionsAndroid } from 'react-native';
 import { fork, put, call } from 'redux-saga/effects';
 import { updateRestaurant } from '../actions/index';
 
@@ -5,8 +6,12 @@ import { fetchRestaurant } from '../api/gnaviApi';
 
 function* initLoad() {
   try {
-    const rest = yield call(fetchRestaurant);
-    yield put(updateRestaurant(rest));
+    // TODO: put localtion param
+    const result = yield call(PermissionsAndroid.request, PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    if (result === PermissionsAndroid.RESULTS.GRANTED) {
+      const rest = yield call(fetchRestaurant);
+      yield put(updateRestaurant(rest));
+    }
   } catch (error) {
     yield console.log(error);
   }
