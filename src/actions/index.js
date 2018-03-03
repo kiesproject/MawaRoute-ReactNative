66f52ab6645b26f2e360f065e082000bdb1963b1
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-03 19:09:13 +0900

    Add action and store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
 const UPDATE_RESTAURANT = 'UPDATE_RESTAURANT';
+const UPDATE_LOCATION = 'UPDATE_LOCATION';
 
 export function updateRestaurant(restraurantList) {
   return {
     type: UPDATE_RESTAURANT,
     list: restraurantList,
+  };
+}
+
+export function updateLocation(location) {
+  return {
+    type: UPDATE_LOCATION,
+    location,
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,6 +12,21 @@ function nav(state = initialNavState, action) {
   }
 }
 
+const initialLocationState = {
+  latitude: 0.0,
+  longitude: 0.0,
+};
+function location(state = initialLocationState, action) {
+  switch (action.type) {
+    case 'UPDATE_LOCATION': {
+      return action.location;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
 const initialListState = List([]);
 function restaurant(state = initialListState, action) {
   switch (action.type) {
@@ -26,6 +41,7 @@ function restaurant(state = initialListState, action) {
 
 const appReducer = combineReducers({
   nav,
+  location,
   restaurant,
 });
 
