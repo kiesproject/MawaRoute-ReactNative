f677ac14e16f4008b001aa6d39aaa0c499ccdf6f
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-03 13:57:56 +0900

    Add getting Location

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,15 +1,16 @@
 import { PermissionsAndroid } from 'react-native';
 import { fork, put, call } from 'redux-saga/effects';
-import { updateRestaurant } from '../actions/index';
 
-import { fetchRestaurant } from '../api/gnaviApi';
+import { updateRestaurant } from '../actions/index';
+import { fetchRestaurantByLocation } from '../api/gnaviApi';
+import { getOnceLocation } from '../gps/LocationManager';
 
 function* initLoad() {
   try {
-    // TODO: put localtion param
     const result = yield call(PermissionsAndroid.request, PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
     if (result === PermissionsAndroid.RESULTS.GRANTED) {
-      const rest = yield call(fetchRestaurant);
+      const location = yield call(getOnceLocation);
+      const rest = yield call(fetchRestaurantByLocation, location);
       yield put(updateRestaurant(rest));
     }
   } catch (error) {
