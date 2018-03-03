473e521a1a4ad8435c1f829b7aeb3d8c465d7762
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-03 19:07:34 +0900

    Add GoogleMap setup for each platform

--- ios/MawaRouteForRN/AppDelegate.m
+++ ios/MawaRouteForRN/AppDelegate.m
@@ -12,10 +12,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAWP-Xgw-ESmkUOea2KUv0VGd1vg7COyZ8"];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
