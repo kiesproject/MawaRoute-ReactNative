0d551627326e05556ef31980035239d42968fd2d
ayatk <git@ayatk.com>
2018-03-05 14:34:08 +0900

    Fix to use world geodetic system

--- src/api/gnaviApi.js
+++ src/api/gnaviApi.js
@@ -3,7 +3,7 @@ const key = '0ec809ca9e26ce462ed18ddc74768cb8';
 const format = 'json';
 
 export function fetchRestaurantByLocation(location) {
-  return fetch(`${response}?keyid=${key}&format=${format}&coordinates_mode=2&latitude=${location.latitude}&longitude=${location.longitude}`)
+  return fetch(`${response}?keyid=${key}&format=${format}&input_coordinates_mode=2&coordinates_mode=2&latitude=${location.latitude}&longitude=${location.longitude}`)
     .then(res => res.json())
     .then(json => json.rest)
     .catch(error => error);
