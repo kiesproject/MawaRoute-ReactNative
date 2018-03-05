62cf3b05ded5b15a707f88a37f24c81dd5951b67
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-05 23:12:03 +0900

    Resolve linter error

--- src/api/gnaviApi.js
+++ src/api/gnaviApi.js
@@ -2,9 +2,12 @@ const response = 'https://api.gnavi.co.jp/RestSearchAPI/20150630/';
 const key = '0ec809ca9e26ce462ed18ddc74768cb8';
 const format = 'json';
 
+const responseWithParam = location => `${response}?keyid=${key}&format=${format}&input_coordinates_mode=2&coordinates_mode=2\
+&latitude=${location.latitude}\
+&longitude=${location.longitude}`;
+
 export function fetchRestaurantByLocation(location) {
-  return fetch(`${response}?keyid=${key}&format=${format}&input_coordinates_mode=2&coordinates_mode=2&latitude=${location.latitude}&longitude=${location.longitude}`)
+  return fetch(responseWithParam(location))
     .then(res => res.json())
-    .then(json => json.rest)
-    .catch(error => error);
+    .then(json => json.rest);
 }
