386975c0056ca322ae75471276c248e0398f4202
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-08 21:54:02 +0900

    Remove dependencies of `react-native-material-kit`

--- android/app/src/main/java/com/mawarouteforrn/MainApplication.java
+++ android/app/src/main/java/com/mawarouteforrn/MainApplication.java
@@ -3,7 +3,6 @@ package com.mawarouteforrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactMaterialKitPackage(),
           new MapsPackage()
       );
     }
